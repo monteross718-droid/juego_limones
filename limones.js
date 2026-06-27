cfb06924c024b0b2f5dde4f2d19a5fe95ebec43a
monteross718-droid <monteross718@gmail.com>
2026-06-26 20:24:36 -0500

    mover a al izquierda

--- limones.js
+++ limones.js
@@ -4,6 +4,7 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2
 
 function iniciar(){
 dibujarSuelo();
@@ -13,11 +14,31 @@ dibujarPersonaje();
 
 function dibujarSuelo (){
     ctx.fillStyle="blue";
-    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.Width,ALTURA_SUELO);
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
 }
 
 function dibujarPersonaje(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+   
+
+}
+
+//mover derecha 
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+
+}
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 
 }
