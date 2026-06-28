a29e250477c374680b8d688414acdf9e56eaa6b0
monteross718-droid <monteross718@gmail.com>
2026-06-28 14:02:59 -0500

    agregar bajar limon

--- limones.js
+++ limones.js
@@ -4,11 +4,17 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+const ANCHO_LIMON=20;
+const ALTO_LIMON=20;
+
 let personajeX=canvas.width/2
+let limonX=canvas.width/2;
+let limonY=5;
 
 function iniciar(){
 dibujarSuelo();
 dibujarPersonaje();
+dibujarLimon();
 
 }
 
@@ -32,13 +38,30 @@ function moverIzquierda(){
 
 //mover derecha 
 
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
+}
+
 function actualizarPantalla(){
     limpiarCanva();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 
 }
 function limpiarCanva(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+
+}
+
+function dibujarLimon(){
+    ctx.fillStyle="green";
+    ctx.fillRect(limonX,limonY ,ANCHO_LIMON,ALTO_LIMON);
+}
+
+function bajarLimon(){
+limonY = limonY + 10;
+actualizarPantalla();
 
 }
